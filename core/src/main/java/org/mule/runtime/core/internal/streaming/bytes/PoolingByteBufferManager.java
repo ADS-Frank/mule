40500f93cdc276c73ed68824a069ed27181667f7
Mariano Gonzalez <mariano.gonzalez@mulesource.com>
2017-05-17 17:57:47 -0300

    experimenting

--- core/src/main/java/org/mule/runtime/core/internal/streaming/bytes/PoolingByteBufferManager.java
+++ core/src/main/java/org/mule/runtime/core/internal/streaming/bytes/PoolingByteBufferManager.java
@@ -7,24 +7,19 @@
 package org.mule.runtime.core.internal.streaming.bytes;
 
 import static java.util.concurrent.TimeUnit.MINUTES;
-import static org.apache.commons.pool.impl.GenericObjectPool.DEFAULT_MAX_IDLE;
-import static org.apache.commons.pool.impl.GenericObjectPool.DEFAULT_MAX_WAIT;
-import static org.apache.commons.pool.impl.GenericObjectPool.WHEN_EXHAUSTED_GROW;
 import static org.mule.runtime.api.i18n.I18nMessageFactory.createStaticMessage;
 import static org.slf4j.LoggerFactory.getLogger;
 import org.mule.runtime.api.exception.MuleRuntimeException;
 import org.mule.runtime.api.lifecycle.Disposable;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.cache.RemovalListener;
-
 import java.nio.ByteBuffer;
 
-import org.apache.commons.pool.ObjectPool;
-import org.apache.commons.pool.PoolableObjectFactory;
-import org.apache.commons.pool.impl.GenericObjectPool;
+import org.apache.commons.pool2.BaseKeyedPooledObjectFactory;
+import org.apache.commons.pool2.KeyedObjectPool;
+import org.apache.commons.pool2.PooledObject;
+import org.apache.commons.pool2.impl.DefaultPooledObject;
+import org.apache.commons.pool2.impl.GenericKeyedObjectPool;
+import org.apache.commons.pool2.impl.GenericKeyedObjectPoolConfig;
 import org.slf4j.Logger;
 
 /**
@@ -41,23 +36,7 @@ public class PoolingByteBufferManager implements ByteBufferManager, Disposable {
   private static final Logger LOGGER = getLogger(PoolingByteBufferManager.class);
   private static final long ONE_MINUTE = MINUTES.toMillis(1);
 
-  private final LoadingCache<Integer, ObjectPool<ByteBuffer>> pools = CacheBuilder.newBuilder()
-      .expireAfterAccess(1, MINUTES)
-      .removalListener((RemovalListener<Integer, ObjectPool<ByteBuffer>>) notification -> {
-        try {
-          notification.getValue().close();
-        } catch (Exception e) {
-          if (LOGGER.isDebugEnabled()) {
-            LOGGER.debug("Found exception trying to dispose buffer pool for capacity " + notification.getKey(), e);
-          }
-        }
-      }).build(new CacheLoader<Integer, ObjectPool<ByteBuffer>>() {
-
-        @Override
-        public ObjectPool<ByteBuffer> load(Integer capacity) throws Exception {
-          return createPool(capacity);
-        }
-      });
+  private final KeyedObjectPool<Integer, ByteBuffer> pool = createPool();
 
   /**
    * {@inheritDoc}
@@ -65,7 +44,7 @@ public class PoolingByteBufferManager implements ByteBufferManager, Disposable {
   @Override
   public ByteBuffer allocate(int capacity) {
     try {
-      return pools.getUnchecked(capacity).borrowObject();
+      return pool.borrowObject(capacity);
     } catch (Exception e) {
       throw new MuleRuntimeException(createStaticMessage("Could not allocate byte buffer"), e);
     }
@@ -76,71 +55,61 @@ public class PoolingByteBufferManager implements ByteBufferManager, Disposable {
    */
   @Override
   public void deallocate(ByteBuffer byteBuffer) {
-    int capacity = byteBuffer.capacity();
-    ObjectPool<ByteBuffer> pool = pools.getIfPresent(capacity);
-    if (pool != null) {
-      try {
-        pool.returnObject(byteBuffer);
-      } catch (Exception e) {
-        if (LOGGER.isDebugEnabled()) {
-          LOGGER.debug("Could not deallocate buffer of capacity " + capacity, e);
-        }
+    try {
+      pool.returnObject(byteBuffer.capacity(), byteBuffer);
+    } catch (Exception e) {
+      if (LOGGER.isWarnEnabled()) {
+        LOGGER.warn("Could not deallocate buffer of capacity " + byteBuffer.capacity(), e);
       }
     }
   }
 
   @Override
   public void dispose() {
-    pools.invalidateAll();
+    try {
+      pool.clear();
+    } catch (Exception e) {
+      if (LOGGER.isWarnEnabled()) {
+        LOGGER.warn("Error disposing pool of byte buffers", e);
+      }
+    }
   }
 
-  private ObjectPool<ByteBuffer> createPool(int capacity) {
-    GenericObjectPool.Config config = new GenericObjectPool.Config();
-    config.maxIdle = DEFAULT_MAX_IDLE;
-    config.maxActive = -1;
-    config.maxWait = DEFAULT_MAX_WAIT;
-    config.whenExhaustedAction = WHEN_EXHAUSTED_GROW;
-    config.minEvictableIdleTimeMillis = ONE_MINUTE;
-    config.timeBetweenEvictionRunsMillis = ONE_MINUTE;
-    config.testOnBorrow = false;
-    config.testOnReturn = false;
-    config.testWhileIdle = false;
-    GenericObjectPool genericPool = new GenericObjectPool(new ByteBufferObjectFactory(capacity), config);
+  private KeyedObjectPool<Integer, ByteBuffer> createPool() {
+    GenericKeyedObjectPoolConfig config = new GenericKeyedObjectPoolConfig();
+    config.setMaxTotalPerKey(-1);
+    config.setBlockWhenExhausted(true);
+    config.setTestOnBorrow(false);
+    config.setSoftMinEvictableIdleTimeMillis(ONE_MINUTE);
+    config.setTimeBetweenEvictionRunsMillis(ONE_MINUTE);
+    config.setTestOnReturn(false);
+    config.setTestWhileIdle(false);
 
-    return genericPool;
-  }
+    return new GenericKeyedObjectPool<>(new BaseKeyedPooledObjectFactory<Integer, ByteBuffer>() {
 
-  private class ByteBufferObjectFactory implements PoolableObjectFactory<ByteBuffer> {
+      @Override
+      public ByteBuffer create(Integer capacity) throws Exception {
+        return ByteBuffer.allocate(capacity);
+      }
 
-    private final int capacity;
+      @Override
+      public PooledObject<ByteBuffer> wrap(ByteBuffer value) {
+        return new DefaultPooledObject<>(value);
+      }
 
-    private ByteBufferObjectFactory(int capacity) {
-      this.capacity = capacity;
-    }
+      @Override
+      public void activateObject(Integer key, PooledObject<ByteBuffer> p) throws Exception {
+        clear(p);
+      }
 
-    @Override
-    public ByteBuffer makeObject() throws Exception {
-      return ByteBuffer.allocate(capacity);
-    }
+      @Override
+      public void passivateObject(Integer key, PooledObject<ByteBuffer> p) throws Exception {
+        clear(p);
+      }
 
-    @Override
-    public void destroyObject(ByteBuffer obj) throws Exception {
-      obj.clear();
-    }
-
-    @Override
-    public boolean validateObject(ByteBuffer obj) {
-      return false;
-    }
-
-    @Override
-    public void activateObject(ByteBuffer obj) throws Exception {
-      obj.clear();
-    }
-
-    @Override
-    public void passivateObject(ByteBuffer obj) throws Exception {
-
-    }
+      private void clear(PooledObject<ByteBuffer> p) {
+        p.getObject().clear();
+      }
+    }, config);
   }
 }
